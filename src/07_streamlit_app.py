dd2a9e2185dc8c1260541d9a9c94de6e7feb233d
Jose Alamo <alamose2@gmail.com>
2024-02-07 20:44:36 +0100

    format website

--- src/07_streamlit_app.py
+++ src/07_streamlit_app.py
@@ -4,7 +4,7 @@ from pickle import load
 from input_transformer import InputTransformer
 from keras.models import load_model
 
-st.title('Hello World')
+st.title('CS GO Match predictor')
 
 transformer = pd.read_pickle('../models/input_transformer.pkl')
 
@@ -18,26 +18,38 @@ class_dict = {
 
 player_options = transformer.player_data['player_nick'].drop_duplicates().sort_values()
 
-player1_team1 = st.selectbox(label='Player 1 Team 1', index=None, options=player_options, placeholder='Please choose a player...')
-player2_team1 = st.selectbox(label='Player 2 Team 1', index=None, options=player_options, placeholder='Please choose a player...')
-player3_team1 = st.selectbox(label='Player 3 Team 1', index=None, options=player_options, placeholder='Please choose a player...')
-player4_team1 = st.selectbox(label='Player 4 Team 1', index=None, options=player_options, placeholder='Please choose a player...')
-player5_team1 = st.selectbox(label='Player 5 Team 1', index=None, options=player_options, placeholder='Please choose a player...')
+st.header('Team 1')
 
-player1_team2 = st.selectbox(label='Player 1 Team 2', index=None, options=player_options, placeholder='Please choose a player...')
-player2_team2 = st.selectbox(label='Player 2 Team 2', index=None, options=player_options, placeholder='Please choose a player...')
-player3_team2 = st.selectbox(label='Player 3 Team 2', index=None, options=player_options, placeholder='Please choose a player...')
-player4_team2 = st.selectbox(label='Player 4 Team 2', index=None, options=player_options, placeholder='Please choose a player...')
-player5_team2 = st.selectbox(label='Player 5 Team 2', index=None, options=player_options, placeholder='Please choose a player...')
+player1_team1 = st.selectbox(label='1', key='Player 1 Team 1', index=None, options=player_options, placeholder='Please choose a player...', label_visibility='collapsed')
+player2_team1 = st.selectbox(label='2', key='Player 2 Team 1', index=None, options=player_options, placeholder='Please choose a player...', label_visibility='collapsed')
+player3_team1 = st.selectbox(label='3', key='Player 3 Team 1', index=None, options=player_options, placeholder='Please choose a player...', label_visibility='collapsed')
+player4_team1 = st.selectbox(label='4', key='Player 4 Team 1', index=None, options=player_options, placeholder='Please choose a player...', label_visibility='collapsed')
+player5_team1 = st.selectbox(label='5', key='Player 5 Team 1', index=None, options=player_options, placeholder='Please choose a player...', label_visibility='collapsed')
+
+st.header('Team 2')
+
+player1_team2 = st.selectbox(label='1', key='Player 1 Team 2', index=None, options=player_options, placeholder='Please choose a player...', label_visibility='collapsed')
+player2_team2 = st.selectbox(label='2', key='Player 2 Team 2', index=None, options=player_options, placeholder='Please choose a player...', label_visibility='collapsed')
+player3_team2 = st.selectbox(label='3', key='Player 3 Team 2', index=None, options=player_options, placeholder='Please choose a player...', label_visibility='collapsed')
+player4_team2 = st.selectbox(label='4', key='Player 4 Team 2', index=None, options=player_options, placeholder='Please choose a player...', label_visibility='collapsed')
+player5_team2 = st.selectbox(label='5', key='Player 5 Team 2', index=None, options=player_options, placeholder='Please choose a player...', label_visibility='collapsed')
 
 
 if st.button("Predict"):
-    transformed_input = transformer.transform_input(
-        [player1_team1, player2_team1, player3_team1, player4_team1, player5_team1],
-        [player1_team2, player2_team2, player3_team2, player4_team2, player5_team2])
+    team1 = [player1_team1, player2_team1, player3_team1, player4_team1, player5_team1]
+    team2 = [player1_team2, player2_team2, player3_team2, player4_team2, player5_team2]
 
-    prediction = str(int(model.predict(transformed_input)[0][0]))
+    players = team1+team2
 
-    pred_class = class_dict[prediction]
+    if any(not isinstance(elem, str) for elem in players):
+        st.error('Please select all players', icon="🚨")
+    elif len(set(players)) < len(players):
+        st.error('Please make sure all players are different', icon="🚨")
+    else:
+        transformed_input = transformer.transform_input(team1, team2)
 
-    st.write("The winner is:", pred_class)
+        prediction = str(int(model.predict(transformed_input)[0][0]))
+
+        pred_class = class_dict[prediction]
+
+        st.write("The winner is:", pred_class)
